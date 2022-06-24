1a04598d8d36a881ea89a5ceae86886e84810ffb
linnchaean <106298174+linnchaean@users.noreply.github.com>
2022-06-24 11:40:09 +0900

    220624_upload

--- common.js
+++ common.js
@@ -56,3 +56,40 @@
          event.preventDefault()
          $('header .header_menu').toggleClass('active')
       });
+
+//order
+      $('main .order thead tr th img').on('click',function(){
+         event.preventDefault()
+         $('main .order tbody tr:nth-child(1)').toggleClass('active')
+         $('main .order tbody tr:nth-child(2)').toggleClass('active')
+         $('main .order tbody tr:last-of-type').toggleClass('active')
+      });
+
+//popup
+      $('main .order-6 tbody tr:nth-child(2) td a').on('click',function(){
+         $('main .shadow').addClass('active')
+         $('main .popup').addClass('active')
+         $('main .popup h6').on('click',function(){
+            $('main .shadow').removeClass('active')
+         $('main .popup').removeClass('active')
+         });
+      });
+
+      $('main .order-6 tbody tr:nth-child(3) td a').on('click',function(){
+         $('main .shadow').addClass('active')
+         $('main .popup-2').addClass('active')
+         $('main .popup-2 h6').on('click',function(){
+            $('main .shadow').removeClass('active')
+         $('main .popup-2').removeClass('active')
+         });
+      });
+
+      $('main .order-6 tbody tr:nth-child(4) td a').on('click',function(){
+         $('main .shadow').addClass('active')
+         $('main .popup-3').addClass('active')
+         $('main .popup-3 h6').on('click',function(){
+            $('main .shadow').removeClass('active')
+         $('main .popup-3').removeClass('active')
+         });
+      })
+
